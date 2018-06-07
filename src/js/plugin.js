0ebc862763f34f2326e7e18220ad38147a96eebc
nteetor <nathanteetor@gmail.com>
2018-06-07 18:40:19 -0400

    upates and bug fixes
    * `data` argument removed, use `{ "data": .. }`
    * fixed pagination bugs
    * improved pagination approach
    * added responsive tables, use `{ "responsive": true }`
    * get selected column values, `.table("selected")`
    * styling improvements
    * editing bug fixes

--- src/js/plugin.js
+++ src/js/plugin.js
@@ -1,149 +1,186 @@
 (function($) {
-  $.fn.table = function(data, config) {
-    const matches = (el, selector) => {
-      return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
-    };
+  $.fn.table = function(config) {
+    config = config || {};
 
-    if (!this.length) {
-      throw ".table() called on invalid element";
+    if (config === "selected") {
+      let $table = this;
+      let selected = $table[0].querySelectorAll("colgroup .selected");
+
+      if (!selected.length) {
+        return [];
+      }
+
+      let indeces = {};
+      Array.prototype.forEach.call(selected, el => {
+        indeces[el.dataset.variable] = Array.prototype.indexOf.call($table[0].querySelectorAll("colgroup col"), el);
+      });
+
+      return Array.prototype.map.call(
+        $table[0].querySelectorAll("tbody tr:not([aria-hidden])"),
+        tr => {
+          let o = {};
+          Object.keys(indeces).forEach(k => {
+            o[k] = tr.querySelector(`td:nth-child(${ indeces[k] + 1 })`).firstChild.value;
+          });
+          return o;
+        }
+      );
     }
 
-    if (!this.is("table")) {
-      throw ".table() must be called on table element";
+
+    if (!(config.data && config.data.length)) {
+      return;
     }
 
-    var options = $.extend(true, {
-      headers: data.length ? Object.keys(data[0]) : null,
-      paginate: 10,
-      nav: {
-        items: 5,
-        prev: "&lang;",
-        next: "&rang;"
+    let options = $.extend(true, {
+      "headers": config.data && config.data.length ? Object.keys(config.data[0]) : [],
+      "paginate": 10,
+      "responsive": false,
+      "filled": true,
+      "nav": {
+        "items": 5,
+        "prev": "&lang;",
+        "next": "&rang;",
+        "align": "center"
       }
     }, config);
 
-    var plugin = this;
-    var nav = {
-      el: null,
-      from: 0,
-      to: options.nav.items
-    };
-    var editing = null;
+    options.data = options.data || [];
 
-    var keycode = {
+    let $plugin;
+    let $nav;
+    let editing;
+
+    let keycode = {
       ENTER: 13
     };
 
-    /*
-     * Create table cells
-     */
-    plugin.addClass("chabudai");
+    if (this[0].tagName.match(/table/i)) {
+      $plugin = this;
+    } else {
+      $plugin = $("<table>", { "class": options["class"] });
+      this.append($plugin);
+    }
 
-    plugin.append(
+    $plugin[0].classList.add("table-chabudai");
+
+    // clear out old contents
+    $plugin.empty();
+
+    if ($plugin[0].nextElementSibling &&
+        $plugin[0].nextElementSibling.tagName.match(/nav/i) &&
+        $plugin[0].nextElementSibling.firstChild &&
+        $plugin[0].nextElementSibling.firstChild.classList.contains("pagination")) {
+      $plugin[0].parentNode.removeChild($plugin[0].nextElementSibling);
+    }
+
+    /*
+     * Table contents
+     */
+    $plugin.append(
+      $("<colgroup>").append(
+        ["#", ...options.headers].map(h => {
+          return $("<col>", {
+            "span": 1,
+            "data-variable": h
+          });
+        })
+      )
+    );
+
+    $plugin.append(
       $("<thead>").append(
         $("<tr>").append(
-          $("<th>", { scope: "col" }).html("#"),
-          $.map(options.headers, x => `<th scope="col">${ x }</th>`)
+          ["#", ...options.headers].map(x => $(`<th scope="col">${ x }</th>`))
         )
       )
     );
 
-    plugin.append(
+    $plugin.append(
       $("<tbody>").append(
-        $.map(
-          data,
-          (r, i) => $("<tr>").append(
-            $("<th>", {
-              scope: "row"
-            }).html(i),
-            Object.values(r).map(c => {
-              var cell;
+        options.data.map((row, index) => {
+          return $("<tr>").append(
+            $("<th>", { scope: "row" })
+              .html(index + 1),
+            Object.values(row).map(v => {
+              if (Array.isArray(v)) {
+                if (v.length === 0) {
+                  return $("<td></td>");
+                }
 
-              if (Array.isArray(c)) {
-                var options = c.map(x => `<option value="${ x }">${ x }</option>`).join("");
-                cell = `<select class="custom-select">${ options }</select>`;
+                let options = v.map(x => `<option value="${ x }">${ x }</option>`).join("");
+                return $(`<td><select class="custom-select">${ options }</select></td>`);
               } else {
-                cell = `<input class="form-control" type="text" value="${ c }" readonly></input>`;
+                return $(`<td><input type="text" value="${ v }"></td>`);
               }
-
-              return `<td>${ cell }</td>`;
             })
-          )
-        )
+          );
+        })
       )
     );
+
+    if (options.filled && options.data.length % options.paginate != 0) {
+      let fillers = Array(options.paginate - (options.data.length % options.paginate));
+      for (let i = 0; i < fillers.length; i++) {
+        fillers[i] = $("<tr aria-hidden><th>&nbsp;</th></tr>");
+      }
+      $plugin.find("tbody").append(fillers);
+    }
 
     /*
-     * Helper functions
+     * Editing
      */
     var stopEditing = function() {
-      if (editing === null) {
+      if (!editing) {
         return;
       }
 
-      if (matches(editing, "td")) {
-        var input = editing.firstChild;
-
-        if (input.tagName === "INPUT") {
-          input.setAttribute("readonly", "");
-        }
-
-        editing.classList.remove("editing", "table-primary");
-        editing = null;
-
-        return;
-      }
-
-      if (matches(editing, "tr")) {
+      if (editing.tagName.match(/td/i)) {
+        editing.classList.remove("editing");
+      } else if (editing.tagName.match(/tr/i)) {
         let input = editing.querySelector("td textarea");
         let values = Object.values(JSON.parse(input.value));
 
         editing.removeChild(input.parentNode);
-        Array.from(editing.getElementsByTagName("td")).forEach((td, i) => {
+        Array.prototype.forEach.call(editing.getElementsByTagName("td"), (td, i) => {
           td.style = null;
           td.firstChild.value = values[i];
         });
 
-        editing = null;
-
-        return;
+        editing.classList.remove("editing");
       }
+
+      editing = null;
     };
 
     const startEditing = function(el) {
-      if (matches(el, "td")) {
-        editing = el;
-        var input = editing.firstChild;
-
-        if (input.tagName === "INPUT") {
-          input.focus();
-          input.selectionStart = input.selectionEnd = input.value.length;
-          input.removeAttribute("readonly");
-        }
-
-        editing.classList.add("editing", "table-primary");
+      if (editing) {
         return;
       }
 
-      if (matches(el, `th[scope="row"]`)) {
+      if (el.tagName.match(/td/i)) {
+        editing = el;
+      } else if (el.tagName.match(/th/i)) {
         editing = el.parentNode;
 
-        let cells = Array.from(editing.getElementsByTagName("td"));
-        let values = cells.map(el => el.firstChild.value);
-        let headers = plugin.find(`thead th[scope="col"]`).get().map(el => el.innerHTML).slice(1);
+        let cells = editing.getElementsByTagName("td");
+        let values = Array.prototype.map.call(cells, el => el.firstChild.value);
+        let headers = Array.prototype.map.call(
+          $plugin[0].querySelectorAll(`thead th[scope="col"]`),
+          el => el.innerHTML
+        );
 
         let map = {};
-        headers.forEach((h, i) => map[h] = values[i]);
+        headers.slice(1).forEach((h, i) => map[h] = values[i]);
 
-        cells.forEach(el => el.style.display = "none");
+        Array.prototype.forEach.call(cells, el => el.style.display = "none");
 
         $(editing.firstChild).after(
           $("<td>", {
-            "class": "editing table-primary",
+            "class": "editing",
             "colspan": cells.length
           }).append(
             $("<textarea>", {
-              "class": "form-control",
               "rows": cells.length + 2
             }).val(JSON.stringify(map, null, 2))
           )
@@ -153,14 +190,62 @@
         textarea.focus();
         textarea.selectionStart = textarea.selectionEnd = textarea.value.length;
       }
+
+      editing.classList.add("editing");
     };
 
+    const clickInside = function(e) {
+      if ($plugin[0].contains(e.target)) {
+        $plugin[0].removeEventListener("click", clickInside);
+        document.addEventListener("click", clickOutside);
+      }
+    };
+
+    const clickOutside = function(e) {
+      if (!$plugin[0].contains(e.target)) {
+        stopEditing();
+        document.removeEventListener("click", clickOutside);
+        $plugin[0].addEventListener("click", clickInside);
+      }
+    };
+
+    $plugin[0].addEventListener("click", clickInside);
+
+    $plugin.on("click", "td:not(.editing)", function(e) {
+      stopEditing();
+      startEditing(this);
+    });
+
+    $plugin.on("click", `th[scope="row"]`, function(e) {
+      if (!this.parentNode.classList.contains("editing")) {
+        stopEditing();
+        startEditing(this);
+      }
+    });
+
+    $plugin.on("keypress", "td.editing", function(e) {
+      if (e.which == keycode.ENTER) {
+        stopEditing();
+      }
+    });
+
+    $plugin.on("click", `th[scope="col"]:not(:first-child)`, function(e) {
+      stopEditing();
+
+      $plugin[0].querySelector(`colgroup col[data-variable="${ this.innerHTML }"]`).classList.toggle("selected");
+
+      $plugin.trigger($.Event("chabudai:select", { "target": this }));
+    });
+
+    /*
+     * Pagination
+     */
     const pageTo = function(li) {
-      if (nav.el === null) {
+      if ($nav === null) {
         return;
       }
 
-      let active = nav.el[0].querySelector(".active");
+      let active = $nav[0].querySelector(".active");
 
       if (active) {
         active.classList.remove("active");
@@ -169,7 +254,7 @@
       li.classList.add("active");
 
       let page = li.dataset.page;
-      let rows = plugin[0].querySelector("tbody").getElementsByTagName("tr");
+      let rows = $plugin[0].tBodies[0].rows; // querySelector("tbody").getElementsByTagName("tr");
 
       for (let i = 0; i < rows.length; i++) {
         rows[i].style.display = "none";
@@ -181,143 +266,108 @@
     };
 
     const navNext = function() {
-      let items = nav.el.find("li:not(:first-child):not(:last-child)");
+      $nav[0].querySelector("li:first-child").classList.remove("disabled");
 
-      if (nav.to >= items.length) {
-        return;
+      let items = $nav[0].querySelectorAll("li:not(:first-child):not(:last-child)");
+
+      let i = 0;
+      let n = 0;
+
+      while (items[i].classList.contains("d-none")) i++;
+
+      while (i < (items.length - options.nav.items) && n < options.nav.items) {
+        items[i].classList.add("d-none");
+        n++;
+        i++;
       }
 
-      if (nav.from == 0) {
-        nav.el[0].querySelector("li:first-child").classList.remove("disabled");
+      n = 0;
+      while (i < items.length && n < options.nav.items) {
+        items[i].classList.remove("d-none");
+        n++;
+        i++;
       }
 
-      nav.from = nav.from + options.nav.items;
-      nav.to = nav.to + options.nav.items;
-
-      items.each((i, el) => {
-        i >= nav.from && i < nav.to ? el.style = null : el.style.display = "none";
-      });
-
-      if (nav.to >= items.length) {
-        nav.el[0].querySelector("li:last-child").classList.add("disabled");
+      if (!items[items.length - 1].classList.contains("d-none")) {
+        $nav[0].querySelector("li:last-child").classList.add("disabled");
       }
     };
 
     const navPrev = function() {
-      let items = nav.el.find("li:not(:first-child):not(:last-child)");
+      $nav[0].querySelector("li:last-child").classList.remove("disabled");
 
-      if (nav.from == 0) {
-        return;
+      let items = $nav[0].querySelectorAll("li:not(:first-child):not(:last-child)");
+
+      let i = items.length - 1;
+      let n = 0;
+
+      while (items[i].classList.contains("d-none")) i--;
+
+      while (i >= options.nav.items && n < options.nav.items) {
+        items[i].classList.add("d-none");
+        n++;
+        i--;
       }
 
-      if (nav.to >= items.length) {
-        nav.el[0].querySelector("li:last-child").classList.remove("disabled");
+      n = 0;
+      while (i >= 0 && n < options.nav.items) {
+        items[i].classList.remove("d-none");
+        n++;
+        i--;
       }
 
-      nav.from = nav.from - options.nav.items;
-      nav.to = nav.to - options.nav.items;
-
-      items.each((i, el) => {
-        i >= nav.from && i < nav.to ? el.style = null : el.style.display = "none";
-      });
-
-      if (nav.from == 0) {
-        nav.el[0].querySelector("li:first-child").classList.add("disabled");
-      }
-    };
-
-    const clickInside = function(e) {
-      if (plugin[0].contains(e.target)) {
-        plugin[0].removeEventListener("click", clickInside);
-        document.addEventListener("click", clickOutside);
+      if (!items[0].classList.contains("d-none")) {
+        $nav[0].querySelector("li:first-child").classList.add("disabled");
       }
     };
 
-    const clickOutside = function(e) {
-      if (!plugin[0].contains(e.target)) {
-        stopEditing();
-        document.removeEventListener("click", clickOutside);
-        plugin[0].addEventListener("click", clickInside);
+    if (options.data.length > 10 && options.paginate !== false) {
+      let total = Math.ceil(options.data.length / 10);
+      let items = [];
+      for (let i = 0; i < total; i++) {
+        items[i] = $(`<li class="page-item" data-page="${ i + 1 }"><a class="page-link">${ i + 1 }</a></li>`);
       }
-    };
 
-    /*
-     * Interact/editing events
-     */
-    plugin[0].addEventListener("click", clickInside);
+      $nav = $("<nav>");
 
-    plugin.on("click", `td:not(.editing), th[scope="row"]`, function(e) {
-      stopEditing();
-      startEditing(this);
-    });
-
-    plugin.on("keypress", "td.editing", function(e) {
-      if (e.which == keycode.ENTER) {
-        stopEditing();
-      }
-    });
-
-    plugin.on("click", `th[scope="col"]`, function(e) {
-      stopEditing();
-
-      let index = Array.from(this.parentNode.children).indexOf(this);
-      let column = plugin[0].querySelectorAll(`td:nth-child(${ index + 1 })`);
-
-      for (let i = 0; i < column.length; i++) {
-        column[i].classList.toggle("selected");
-        column[i].classList.toggle("table-primary");
-      }
-    });
-
-    plugin.appendTo(plugin);
-
-    /*
-     * Pagination
-     */
-    if (data.length > 10 && options.paginate !== false) {
-      let total = Math.ceil(data.length / 10);
-      let items = Array.from(
-        Array(total),
-        (_, i) => `<li class="page-item" data-page="${ i + 1 }"><a class="page-link">${ i + 1 }</a></li>`
-      );
-
-      nav.el = $("<nav>").append(
-        $("<ul>", {
-          "class": "pagination justify-content-center"
-        }).append(
-          items.join("")
-        ));
-
+      // if we need to add the next and prev additional nav items
       if (items.length > options.nav.items) {
-        nav.el.find(`li:nth-child(n + ${ 1 + options.nav.items }):nth-child(-n + ${ items.length })`).css("display", "none");
+        for (let j = options.nav.items; j < items.length; j++) {
+          items[j][0].classList.add("d-none");
+        }
 
-        nav.el.find(".pagination").prepend(
-          `<li class="page-item disabled"><a class="page-link">${ options.nav.prev }</a></li>`
-        );
+        items.unshift($(`<li class="page-item disabled"><a class="page-link">${ options.nav.prev }</a></li>`));
+        items.push($(`<li class="page-item"><a class="page-link">${ options.nav.next }</a></li>`));
 
-        nav.el.find(".pagination").append(
-          `<li class="page-item"><a class="page-link">${ options.nav.next }</a></li>`
-        );
-
-        nav.el.on("click", "li:first-child", function(e) {
+        $nav.on("click", "li:first-child:not(.disabled)", function(e) {
           navPrev();
         });
 
-        nav.el.on("click", "li:last-child", function(e) {
+        $nav.on("click", "li:last-child:not(.disabled)", function(e) {
           navNext();
         });
       }
 
-      nav.el.on("click", `li[data-page]:not(.active)`, function(e) {
+      $nav.append($("<ul>", { "class": "pagination justify-content-center" }).append(items));
+
+      $nav.on("click", `li[data-page]:not(.active)`, function(e) {
         e.preventDefault();
         pageTo(this);
       });
 
-      plugin.after(nav.el);
+      $plugin.after($nav);
 
-      pageTo(nav.el[0].querySelector(`li[data-page="1"]`));
+      pageTo($nav[0].querySelector(`li[data-page="1"]`));
     }
 
-    return plugin;
+    /*
+     * Make table responsive
+     */
+    if (options.responsive) {
+      $plugin.wrap(`<div class="table-responsive"></div>`);
+    }
+
+
+    return $plugin;
   };
 }(jQuery));
